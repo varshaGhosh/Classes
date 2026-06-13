6458feb35a488fdd1990a3cd1f625200c6017670
varshaGhosh <dalipghosh22@gmail.com>
2026-06-13 17:57:36 +0530

    Create 5_class_printing date.c

---
+++ 5_class_printing date.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int date = 13;
+    int month = 6;
+    int year = 2026;
+    printf("%d %d %d\n",date,month,year);
+    printf("%d %d %d\n",month,date,year);
+
+}

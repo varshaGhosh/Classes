f8bd520b11aced0141191c2135fc498fe36af41d
varshaGhosh <dalipghosh22@gmail.com>
2026-06-16 18:02:58 +0530

    Create 5_ class_comment.c

---
+++ 5_ class_comment.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+    // declaring an int  named "date"
+    int date = 16;
+    // declaring an int  named "month"
+    int month = 6;
+     // declaring an int  named "year"
+    int year = 2026;
+    // declaring an int named "date""month""year"
+    printf("%d/0%d%/%d\n", date, month, year);
+
+}
